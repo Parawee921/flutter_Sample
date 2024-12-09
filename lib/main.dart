ee9b6ab4c939faf9dc54854386bb5da4cff3d90b
Parawee921 <weawanpoaphang@gmail.com>
2024-12-09 10:43:10 +0700

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/flutter_sample.dart';
+import 'package:flutter_application_1/my_gird_page.dart';
 //import 'package:flutter_application_1/my_contrainer.dart';
-import 'package:flutter_application_1/my_scroll.dart';
+//import 'package:flutter_application_1/my_scroll.dart';
 
 void main() {
   runApp(const MainApp());
@@ -15,7 +16,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'My Flutter Simple',
-      home: FlutterSample()
+      home: MyGirdPage()
     );
   }
 }
